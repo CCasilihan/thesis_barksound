4a1919e1b15a7adccf650dde3277dad2846c07d4
CCasilihan <158982748+CCasilihan@users.noreply.github.com>
2024-02-12 21:07:35 +0800

    Update db.js

--- db.js
+++ db.js
@@ -1,11 +1,30 @@
 // db.js
+// const mysql = require('mysql2');
+
+// const db = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'root',
+//   password: '',
+//   database: 'thesis_barksound'
+// });
+
+// db.connect((err) => {
+//   if (err) {
+//     console.error("error connecting to mysql: ", err)
+//   } else {
+//     console.log("connected to mysql")
+//   }
+// });
+
+// module.exports = db;
+
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'thesis_barksound'
+  host: 'sql6.freemysqlhosting.net',
+  user: 'sql6683531',
+  password: 'K2CM97kUHR',
+  database: 'sql6683531'
 });
 
 db.connect((err) => {
